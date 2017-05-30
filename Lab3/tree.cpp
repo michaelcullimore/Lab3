be519fa9a9b64e6216227bc68dd911b6cccbbdfd
michaelcullimore <michaelcullimore@mail.weber.edu>
2017-05-30 10:42:40 -0600

    set up of first outputs and prompts

--- Lab3/tree.cpp
+++ Lab3/tree.cpp
@@ -6,8 +6,13 @@
 using namespace std;
 
 int main () {
+	cout << "***===***===Lab3 - Tree===***===***\n";
+	cout << "This program makes an ANSII tree with a height specified by the user.\n\n";
 
-	
+	double treeHeight = 0.0;
+
+	cout << "How tall should the tree be?: "; cin >> treeHeight; cout << endl;
+
 	system ("pause");
 	return 0;
 }
