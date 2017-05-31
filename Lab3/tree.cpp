ef89c3da7928f7537f72a19684d6969d98dcced8
michaelcullimore <michaelcullimore@mail.weber.edu>
2017-05-31 10:43:51 -0600

    I am officially stuck. Asking Prof Brinkerhoff for help to see what he has to say.

--- Lab3/tree.cpp
+++ Lab3/tree.cpp
@@ -10,14 +10,32 @@ int main () {
 	cout << "***===***===Lab3 - Tree===***===***\n";
 	cout << "This program makes an ANSII tree with a height specified by the user.\n\n";
 
-	double treeHeight = 0.0;
+	int treeHeight;
 
 	cout << "How tall should the tree be?: "; 
 	cin >> treeHeight; cout << endl;
 
+	//limits of the tree
 	if (treeHeight < 3 || treeHeight > 15) {
 		cout << "Error. Tree dimensions are out of bounds.\n";
-		exit (0);
+		return 0;
+	}
+
+	//Height of tree
+	for (int level = 0; level <= treeHeight; level++) {
+		//prints spaces above tree, leading spaces
+		for (int outerSpaces = 0; outerSpaces < treeHeight - level - 1; outerSpaces++) {
+			cout << " ";
+		}
+		for (int edge = 0; edge <= (2 * level - 1); edge++) {
+			if (level == treeHeight || edge == 1|| edge == (2 * level+1)) {
+				cout << "X";
+			}
+			else {
+				cout << " ";
+			}
+		}
+		cout << endl;
 	}
 
 	system ("pause");
