48a22745aa5453a5e64ae517a97672cf7cf666c4
michaelcullimore <michaelcullimore@mail.weber.edu>
2017-06-01 22:33:40 -0600

    FINSIHED THE PROJECT!!! I FIGURED IT OUT!!! YEE SON!!!

--- Lab3/tree.cpp
+++ Lab3/tree.cpp
@@ -7,50 +7,47 @@
 using namespace std;
 
 int main () {
-	cout << "***===***===Lab3 - Tree===***===***\n";
-	cout << "This program makes an ANSII tree with a height specified by the user.\n\n";
+	cout << "***===***===Lab3 - Tree===***===***" << endl;
+	cout << "This program makes an ANSII tree with a height specified by the user." << endl << endl;
 
 	int treeHeight;
 
 	cout << "How tall should the tree be?: "; 
 	cin >> treeHeight;
 
-	//limits of the tree
 	if (treeHeight < 3 || treeHeight > 15) {
-		cout << "Error. Tree dimensions are out of bounds.\n";
+		cout << "Error. Tree dimensions are out of bounds." << endl;
 		return 0;
 	}
 
-	//Height of tree
 	for (int level = 0; level < treeHeight; level++) {
-		//prints spaces above tree, leading spaces
-		for (int spaces = 0; spaces < treeHeight - level - 1; spaces++) {//good
+		for (int topSpaces = 0; topSpaces < treeHeight - level - 1; topSpaces++) {
 			cout << " ";
 		}
 
-		//prints the forward slashes
 		cout << "/";
 
-		//spaces inside the "triangle"
-		for (int spaces2 = 0; spaces2 < (2 * level); spaces2++) {
+		for (int insideSpaces = 0; insideSpaces < 2 * level; insideSpaces++) {
 			cout << " ";
 		}
 
-		//prints the back slashes
 		cout << "\\";
-
 		cout << endl;
 	}
 
-	//prints the baseline of the triangle
 	for (int baseline = 0; baseline < (treeHeight * 2); baseline++) {
 		cout << "-";
 	}
 
 	cout << endl;
 
-	for (int underSpace = 0; underSpace < (treeHeight - 1); underSpace++) {
-		cout << "x";
+	for (int level = 0; level < (treeHeight / 2); level++) {
+		for (int leftSpace = 0; leftSpace < (treeHeight - 1); leftSpace++) {
+			cout << " ";
+		}
+
+		cout << "||";
+		cout << endl;
 	}
 
 	system ("pause");
