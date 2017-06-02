d905b14fd4f165d1308fc4e9ea9257d8889bf454
michaelcullimore <michaelcullimore@mail.weber.edu>
2017-06-01 20:02:20 -0600

    Got the triangle part figured out. I had it print out way too many slashes on both sides. I had a heck of a time and spent hours trying to figure it out. Thanks Nate.

--- Lab3/tree.cpp
+++ Lab3/tree.cpp
@@ -13,7 +13,7 @@ int main () {
 	int treeHeight;
 
 	cout << "How tall should the tree be?: "; 
-	cin >> treeHeight; cout << endl;
+	cin >> treeHeight;
 
 	//limits of the tree
 	if (treeHeight < 3 || treeHeight > 15) {
@@ -22,21 +22,43 @@ int main () {
 	}
 
 	//Height of tree
-	for (int level = 0; level <= treeHeight; level++) {
+	for (int level = 0; level < treeHeight; level++) {
 		//prints spaces above tree, leading spaces
-		for (int outerSpaces = 0; outerSpaces < treeHeight - level - 1; outerSpaces++) {
+		for (int spaces = 0; spaces < treeHeight - level - 1; spaces++) {//good
 			cout << " ";
 		}
-		for (int edge = 0; edge <= (2 * level - 1); edge++) {
-			if (level == treeHeight || edge == 1|| edge == (2 * level+1)) {
-				cout << "X";
-			}
-			else {
-				cout << " ";
-			}
+
+		//for (int fSlash = 0; fSlash <= (2 * level); fSlash++) {
+			cout << "/";
+		//}
+		for (int spaces2 = 0; spaces2 < (2 * level); spaces2++) {
+			cout << " ";
 		}
+
+
+		//for (int bSlash = 0; bSlash <= (treeHeight - level); bSlash++) {
+			cout << "\\";
+		//}
+
+		/*for (int spaces2 = 0; spaces2 < (treeHeight - level - 1); spaces2++) {
+			cout << " ";
+		}*/
+
+		//should be code for spaces inside the triangle. How do I get them in there?
+		/*for (int baseline = 0; baseline < (level * 2); baseline++) {
+			cout << "-";
+		}*/
+
 		cout << endl;
 	}
+
+	//prints the baseline of the triangle
+	for (int baseline = 0; baseline < (treeHeight * 2); baseline++) {
+		cout << "-";
+	}
+	cout << endl;
+
+	
 
 	system ("pause");
 	return 0;
